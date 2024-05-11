2bf1508f6c360f7f572b64fe4519d5d7be81c921
Jan Seipel <janfseipel@gmail.com>
2024-05-11 07:11:08 +0200

    saved all important data from good article pages in json file

---
+++ wiki-memo/data-loader/data-good-articles.py
@@ -0,0 +1,51 @@
+import json
+import requests
+
+headers={"user-agent":"janseipel", "from":"janfseipel@gmail.com"}
+
+def get_img_url(article_name):
+    img_api_call = "https://de.wikipedia.org/w/api.php?action=query&prop=pageimages&pithumbsize=500&format=json&titles="
+    response = requests.get(img_api_call + article_name, headers=headers)
+    js = json.loads(response.text)
+    img_url = js["query"]["pages"].popitem()[1]["thumbnail"]["source"]
+    return img_url
+
+def get_article_summary(article_name):
+    img_api_call = "https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+    response = requests.get(img_api_call + article_name, headers=headers)
+    js = json.loads(response.text)
+    extract = js["query"]["pages"].popitem()[1]["extract"]
+    return extract
+
+def get_article_data(link):
+    article_name = link.split("/")[2]
+    img_url = get_img_url(article_name)
+    article_summary = get_article_summary(article_name)
+    return article_summary, img_url
+
+with open('first-data-good-articles.json', encoding='utf-8') as f:
+    d = json.load(f)
+
+articles = d[0]["subcategories"][0]["subcat_articles"]
+
+for num_cat, category in enumerate(d):
+
+    print(num_cat, "of", len(d), "-",category["category-title"])
+    subcats = category["subcategories"]
+
+    for num_subcat, subcat in enumerate(subcats):
+        print("----",num_subcat, "of", len(subcats),"-",subcat["subcat_title"])
+        articles = subcat["subcat_articles"]
+
+        for article in articles:
+            print("----","----",article["title"])
+            try:
+                article["summary"], article["img_url"] = get_article_data(article["link"])
+            except:
+                pass
+
+with open('full-data-good-articles.json', 'w', encoding='utf8') as json_file:
+    json.dump(d, json_file, ensure_ascii=False)
+
+
+    
--- wiki-memo/data-loader/extractor-good-articles.py
+++ wiki-memo/data-loader/extractor-good-articles.py
@@ -8,13 +8,7 @@ url = "https://de.wikipedia.org/wiki/Wikipedia:Lesenswerte_Artikel"
 # HTML-Inhalt der Seite herunterladen
 response = requests.get(url)
 html_content = response.content
-
-# HTML-Inhalt mit BeautifulSoup analysieren
 soup = BeautifulSoup(html_content, "html.parser")
-
-# Seitentitel extrahieren
-# title = soup.find("h1", {"class": "firstHeading"}).text.strip()
-# print("Seitentitel:", title)
 
 data = []
 
@@ -22,7 +16,6 @@ data = []
 categories = soup.findAll("span", {"class": "mw-headline"})
 
 for i, category in enumerate(categories):
-    # print(category.text.strip())
 
     # Unterkategorien und zugeordnete Links finden
     cat_table = category.parent.parent.parent.parent.parent.findNext("table")
@@ -35,9 +28,7 @@ for i, category in enumerate(categories):
         subcat_obj["subcat_title"] = subcat_p.find("b").text.strip()[:-1]
         subcat_link_tags = subcat_p.findAll("a")
         subcat_obj["subcat_articles"] = [{"id": k+1, "link":tag["href"],"title":tag.text.strip()} for k, tag in enumerate(subcat_link_tags)]
-
         subcats.append(subcat_obj)
-        # print(subcat_obj)
 
     cat_obj = {
         "id": i+1,
@@ -47,16 +38,5 @@ for i, category in enumerate(categories):
 
     data.append(cat_obj)
 
-#print(json.dumps(data, indent=4))
-
-with open('data.json', 'w', encoding='utf8') as json_file:
+with open('first-data-good-articles.json', 'w', encoding='utf8') as json_file:
     json.dump(data, json_file, ensure_ascii=False)
-
-
-
-
-
-
-# Ersten Absatz extrahieren
-# first_paragraph = soup.find("div", {"class": "mw-parser-output"}).find("p").text.strip()
-# print("Erster Absatz:", first_paragraph)
