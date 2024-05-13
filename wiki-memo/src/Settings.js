050eb546c36d270ab5c4931010b2f1a8befe9e30
Jan Seipel <janfseipel@gmail.com>
2024-05-13 22:08:20 +0200

    Inserted selection menu with categories and subcats as options

--- wiki-memo/src/Settings.js
+++ wiki-memo/src/Settings.js
@@ -1,11 +1,38 @@
-
+import cardData from "./full-data-good-articles.json";
 
 export function Settings({handleClick}) {
 
     return (
         <>
             <h1>Settings first!</h1>
+            <SelectCards />
             <button className="btn btn-primary" onClick={handleClick}>Click</button>
+        </>
+    )
+}
+
+function SelectCards() {
+
+    const options = cardData.map((d)=>{
+
+        const cat = <option value={"cat-id-"+ d.id} style={{"font-weight":"bold"}}>{d.category_title}</option>
+        const subcats = d.subcategories.map((e)=>{
+            return (
+                <option value={"subcat-id-"+e.id}>-- {e.subcat_title}</option>
+            )
+        })
+
+        return (
+            [cat,...subcats]
+        )
+    })
+
+    return (
+        <>
+            <select className="form-select">
+                <option value="" style={{"font-weight":"bold"}}>Alle Kategorien</option>
+                {options}
+            </select>
         </>
     )
 }
